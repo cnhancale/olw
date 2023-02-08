b4386966ab6c56f49b44b227076883476f3a4da4
cnhancale <clnhancale@gmail.com>
2023-02-08 12:30:55 +0200

    add softDeletes

--- database/migrations/2023_02_08_100432_create_sellers_table.php
+++ database/migrations/2023_02_08_100432_create_sellers_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId('company_id')->constrained();
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
