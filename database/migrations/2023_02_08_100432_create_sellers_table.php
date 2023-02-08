436db388657d0a790547d9381746c3e686b6a1ec
cnhancale <clnhancale@gmail.com>
2023-02-08 12:07:32 +0200

    Update 2023_02_08_100432_create_sellers_table.php

--- database/migrations/2023_02_08_100432_create_sellers_table.php
+++ database/migrations/2023_02_08_100432_create_sellers_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('company_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
