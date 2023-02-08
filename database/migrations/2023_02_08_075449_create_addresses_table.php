bc2a560a71a67bb64c6541264b0966f247741639
cnhancale <clnhancale@gmail.com>
2023-02-08 10:11:42 +0200

    Update 2023_02_08_075449_create_addresses_table.php

--- database/migrations/2023_02_08_075449_create_addresses_table.php
+++ database/migrations/2023_02_08_075449_create_addresses_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('city');
+            $table->char('state',2);
             $table->timestamps();
         });
     }
